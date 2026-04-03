7f4c809120a899e7c9a35d69819249f6a1ba4c14
Manuel Cupul <114542854+ManuelC13@users.noreply.github.com>
2026-04-03 16:45:11 -0600

    setup: configuración inicial frontend con Vite y Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-red-500">
+        FarmaUady
+      </h1>
+    </div>
+  );
+}
+
+export default App;
