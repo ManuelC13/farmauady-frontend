a89a0aff1724401771840bfe45ac7719a29bf40a
RaulNahuat <a22216876@alumnos.uady.mx>
2026-04-25 23:23:01 -0600

    feat: configure axios instance with base URL and automatic token refresh interceptor

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,4 +6,26 @@ export const api = axios.create({
         'Content-Type': 'application/json',
     },
     withCredentials: true, // Para poder usar cookies
-})
+});
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    async (error) => {
+        const originalRequest = error.config;
+        
+        if (error.response && error.response.status === 401 && !originalRequest._retry && originalRequest.url !== '/auth/refresh' && originalRequest.url !== '/auth/login') {
+            originalRequest._retry = true;
+            try {
+                console.info("Refrescando token...");
+                await axios.post("http://localhost:8000/auth/refresh", {}, { withCredentials: true });
+                console.info("Token actualizado exitosamente");
+                return api(originalRequest);
+            } catch (refreshError) {
+                return Promise.reject(refreshError);
+            }
+        }
+        return Promise.reject(error);
+    }
+);
